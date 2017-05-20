604c918f95ed61b32c8927bd4fd63bc88e64b2c4
Max Conrad <maxconrad@me.com>
2017-05-20 17:53:58 -0400

    db fixes

--- src/db.php
+++ src/db.php
@@ -1,21 +1,22 @@
 <html> 
     <body> 
         <?php 
-        $db = pg_connect('host=localhost dbname=vmug user=admin password=admin'); 
+        $db = pg_connect('host=localhost dbname=vmug user=Max password=postgres'); 
 
         $firstname = pg_escape_string($_POST['firstname']); 
         $lastname = pg_escape_string($_POST['lastname']); 
-        $emailaddress = pg_escape_string($_POST['email']); 
+        $email = pg_escape_string($_POST['email']); 
         $company = pg_escape_string($_POST['company']);
+        $option = pg_escape_string($_POST['option'])
 
-        $query = "INSERT INTO users(firstname, lastname, email, company) VALUES('" . $firstname . "', '" . $lastname . "', '" . $email . "', '" . $company . "')"; 
+        $query = "INSERT INTO users(firstname, lastname, email, company, option) VALUES('" . $firstname . "', '" . $lastname . "', '" . $email . "', '" . $company . "', '" . $option . "')"; 
         $result = pg_query($query); 
         if (!$result) { 
             $errormessage = pg_last_error(); 
             echo "Error with query: " . $errormessage; 
             exit(); 
         } 
-        printf ("These values were inserted into the database - %s %s %s %s", $firstname, $lastname, $emailaddress, $company); 
+        printf ("These values were inserted into the database - %s %s %s %s %s", $firstname, $lastname, $emailaddress, $company, $option); 
         pg_close(); 
         ?> 
     </body> 
