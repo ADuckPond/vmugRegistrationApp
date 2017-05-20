06331d38260453004b476e08f190262bca7eaf84
Max Conrad <maxconrad@me.com>
2017-05-20 17:10:58 -0400

    db changes

--- src/db.php
+++ src/db.php
@@ -1,14 +1,14 @@
 <html> 
     <body> 
         <?php 
-        $db = pg_connect('host=localhost dbname=contacts user=contacts password=firstphp'); 
+        $db = pg_connect('host=localhost dbname=vmug user=admin password=admin'); 
 
         $firstname = pg_escape_string($_POST['firstname']); 
         $lastname = pg_escape_string($_POST['lastname']); 
         $emailaddress = pg_escape_string($_POST['email']); 
         $company = pg_escape_string($_POST['company']);
 
-        $query = "INSERT INTO friends(firstname, surname, emailaddress) VALUES('" . $firstname . "', '" . $lastname . "', '" . $email . "', '" . $company . "')"; 
+        $query = "INSERT INTO users(firstname, lastname, email, company) VALUES('" . $firstname . "', '" . $lastname . "', '" . $email . "', '" . $company . "')"; 
         $result = pg_query($query); 
         if (!$result) { 
             $errormessage = pg_last_error(); 
