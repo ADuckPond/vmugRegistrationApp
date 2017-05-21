d958d6b919dc4f2dd232b3947034cbb9dd9eb4fa
ADuckPond <ag3rlitz@gmail.com>
2017-05-21 01:34:40 -0400

    removed src from paths and modified db.php based on config tests

--- php/db.php
+++ php/db.php
@@ -1,23 +1,28 @@
-<html> 
-    <body> 
-        <?php 
-        $db = pg_connect('host=localhost dbName=vmug user=Max pwd=postgres'); 
+<html>
+    <body>
+        <?php
+        $dbHost = '172.18.0.100';
+        $dbUser = getenv('DB_USER');
+        $dbPwd = getenv('DB_PWD');
+        $dbName = getenv('DB_NAME');
+        $conn_str = "host=172.18.0.100 dbname=vmug user=postgres password=postgres";
+        $db = pg_connect($conn_str);
 
-        $firstname = pg_escape_string($_POST['firstname']); 
-        $lastname = pg_escape_string($_POST['lastname']); 
-        $email = pg_escape_string($_POST['email']); 
+        $firstname = pg_escape_string($_POST['firstname']);
+        $lastname = pg_escape_string($_POST['lastname']);
+        $email = pg_escape_string($_POST['email']);
         $company = pg_escape_string($_POST['company']);
         $option = pg_escape_string($_POST['option']);
 
-        $query = "INSERT INTO users(firstname, lastname, email, company, option) VALUES('" . $firstname . "', '" . $lastname . "', '" . $email . "', '" . $company . "', '" . $option . "')"; 
-        $result = pg_query($query); 
-        if (!$result) { 
-            $errormessage = pg_last_error(); 
-            echo "Error with query: " . $errormessage; 
-            exit(); 
-        } 
-        printf ("These values were inserted into the database - %s %s %s %s %s", $firstname, $lastname, $emailaddress, $company, $option); 
-        pg_close(); 
-        ?> 
-    </body> 
+        $query = "INSERT INTO members(firstname, lastname, email, company, comptype) VALUES('" . $firstname . "', '" . $lastname . "', '" . $email . "', '" . $company . "', '" . $option . "')";
+        $result = pg_query($query);
+        if (!$result) {
+            $errormessage = pg_last_error();
+            echo "Error with query: " . $errormessage;
+            exit();
+        }
+        printf ("These values were inserted into the database - %s %s %s %s %s", $firstname, $lastname, $emailaddress, $company, $option);
+        pg_close();
+        ?>
+    </body>
 </html>
