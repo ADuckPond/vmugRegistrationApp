b5c0b01790bcfb605fab0c8f744f236237724a38
ADuckPond <ag3rlitz@gmail.com>
2017-05-25 01:41:48 -0400

    php code to create temp table for upload process.

--- php/adminButtons.php
+++ php/adminButtons.php
@@ -3,5 +3,11 @@
 
     if(isset[$_FILES] && $_FILES.count == 1){
 
+        $file=$_FILES;
+
+        $queryCreate = "CREATE TEMPORARY TABLE temp (atend text, name text, company text)";
+        $resultCreate = pg_query($db,$queryCreate);
+
+        $queryCopy = "COPY temp (atend, name, company) FROM '$_FILES' DELIMETER ',' CSV HEADER"; 
     }
 ?>
