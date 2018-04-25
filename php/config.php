9353b4fe9511b1235ea363e893b00199850afb6f
ADuckPond <ag3rlitz@gmail.com>
2018-04-25 19:36:42 -0400

    DB_NAME set to /php/db (couldnt find where).  Using DB_NAME2 as a workaround env variable.

--- php/config.php
+++ php/config.php
@@ -2,7 +2,7 @@
     $dbHost = getenv('DB_HOST');
     $dbUser = getenv('DB_USER');
     $dbPwd = getenv('DB_PWD');
-    $dbName = getenv('DB_NAME');
+    $dbName = getenv('DB_NAME2');
     $conn_str = "host=$dbHost dbname=$dbName user=$dbUser password=$dbPwd";
     $db = pg_connect($conn_str);
 ?>
